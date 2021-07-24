2bedb28222ce2e412326d42cf433c2ccb4899eff
SPARSH <spgl669@gmail.com>
2021-07-24 19:43:27 +0530

    add start script

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -28,7 +28,7 @@ module.exports.createCampground=async(req,res,next)=>{
      campground.geometry=geoData.body.features[0].geometry;
     campground.images=req.files.map(f=>({url:f.path, filename:f.filename}));
     campground.author=req.user._id;
-    console.log(campground);
+    
     await campground.save();
     req.flash('success','you have sucessfully made a campground');
     res.redirect(`/Campground/${campground._id}`); 
