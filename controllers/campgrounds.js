391bd5e710804a3b601117956c38883defe1b6e9
SPARSH <spgl669@gmail.com>
2021-07-25 12:41:17 +0530

    add start script

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -18,7 +18,7 @@ module.exports.renderNewForm=(req,res)=>{
 }
 
 module.exports.createCampground=async(req,res,next)=>{
-    res.send('hello');
+    
     const geoData=await geocoder.forwardGeocode({
         query:req.body.campground.location,
         limit:1
