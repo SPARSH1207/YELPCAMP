8556a713ffde9935da83054dd56ce0fd7b4d0d30
SPARSH <spgl669@gmail.com>
2021-07-26 22:27:26 +0530

    add start script

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,12 +1,16 @@
  const cloudinary=require('cloudinary').v2;
 const {CloudinaryStorage}=require('multer-storage-cloudinary');
 
-
- cloudinary.config({
+cloudinary.config({
+    cloud_name:'dgsqkv8do',
+    api_key:'879951332755225',
+    api_secret:'NHhdtYJqM7UWCkeyUVoPCPK2CH8'
+});
+/* cloudinary.config({
     cloud_name:process.env.ClOUDINARY_CLOUD_NAME,
     api_key:process.env.ClOUDINARY_KEY,
     api_secret:process.env.ClOUDINARY_SECRET
-});
+}); */
 
 
 const storage=new CloudinaryStorage({
